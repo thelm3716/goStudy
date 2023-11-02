e35fc9e28c57c9df9fb84ec22ab381811c077de4
skitttles <thelm3716@naver.com>
2023-11-03 00:37:14 +0900

    Hello, World! - commit

--- first.go
+++ first.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!")
+	i := 10
+
+	if i >= 5 {
+		fmt.Println("5 이상")
+	}
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/thelm3716/goStudy
+
+go 1.21.3
